33623f391bdca48e27e7c4c3aa036da79b65d529
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-11 09:07:37 -0500

    se agrega cambios

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -1,71 +1,71 @@
-const objProduct={};
-const objMysqlQueriesProduct= require('../MysqlQueries/ProductMysqlQueries.js');
-const Helpers=require('./Helpers');
+const objProduct = {};
+const objMysqlQueriesProduct = require("../MysqlQueries/ProductMysqlQueries.js");
+const Helpers = require("./Helpers");
 //Create product
-objProduct.CreateProduct=async(req,res)=>{
-    try{
-        //Guardando img
-        await Helpers.DecodeBase64(req.body.strImgBase64,req.body.intIdProduct);
-        //Create product
-        await objMysqlQueriesProduct.CreateProduct(req.body);
-        res.json({
-            Success:true,
-            strMensaje:"Product creado con éxito."
-        })
-    }catch(Error){
-        console.log(Error);
-    }
-}
+objProduct.CreateProduct = async (req, res) => {
+  try {
+    //Guardando img
+    await Helpers.DecodeBase64(req.body.strImgBase64, req.body.intIdProduct);
+    //Create product
+    await objMysqlQueriesProduct.CreateProduct(req.body);
+    res.json({
+      Success: true,
+      strMensaje: "Product creado con éxito.",
+    });
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 //List product
-objProduct.ListProducts=async(req,res)=>{
-    try{
-        //Query
-        let strDataQuery = await objMysqlQueriesProduct.ListProducts();
-        res.json({
-            Success: true,
-            strData: strDataQuery.strData
-        })
-    }catch(Error){
-        console.log(Error)
-    }
-}
+objProduct.ListProducts = async (req, res) => {
+  try {
+    //Query
+    let strDataQuery = await objMysqlQueriesProduct.ListProducts();
+    res.json({
+      Success: true,
+      strData: strDataQuery.strData,
+    });
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 
 //Edit Product
 objProduct.EditProduct = async (req, res) => {
-    try { 
-       //Guardando img
-        if(req.body.strImgBase64!=null){
-            await Helpers.DecodeBase64(req.body.strImgBase64,req.body.intIdProduct);
-        }
-        //Queries Mysql
-        await objMysqlQueriesProduct.EditProduct(req.body);
-         res.json({
-             Success:true,
-             strMensaje:'Product edit with success'
-         })   
-    } catch (Error) {
-       console.log(Error)
+  try {
+    //Guardando img
+    if (req.body.strImgBase64 != null) {
+      await Helpers.DecodeBase64(req.body.strImgBase64, req.body.intIdProduct);
     }
-}
+    //Queries Mysql
+    await objMysqlQueriesProduct.EditProduct(req.body);
+    res.json({
+      Success: true,
+      strMensaje: "Product edit with success",
+    });
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 //Get product
-objProduct.GetProduct=async(req,res)=>{
-    try{
-        let strDataProduct=await objMysqlQueriesProduct.GetProduct(req.params.strCode);
-        strDataProduct.Success=true;
-        res.json(
-            strDataProduct
-        )
-    }catch(Error){
-        console.log(Error)
-    }
-}
+objProduct.GetProduct = async (req, res) => {
+  try {
+    let strDataProduct = await objMysqlQueriesProduct.GetProduct(
+      req.params.strCode
+    );
+    strDataProduct.Success = true;
+    res.json(strDataProduct);
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 //Get img
 objProduct.GetArchivo = async (req, res) => {
-    try {
-       res.sendFile(req.params.Img, { root: './Public/Products' });
-    } catch (Error) {
-        console.log(Error);
-    }
-}
+  try {
+    res.sendFile(req.params.Img, { root: "./src/Public/Products" });
+  } catch (Error) {
+    console.log(Error);
+  }
+};
 
-module.exports=objProduct;
+module.exports = objProduct;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config({ path: '../.env' });
-const app = require('./app');
+require("dotenv").config({ path: "../.env" });
+const app = require("./app");
 async function Main() {
-    await app.listen(app.get('port'));
-    console.log('server puerto 3000');
+  await app.listen(process.env.PORT || app.get("port"));
+  console.log("server puerto 3000");
 }
 Main();
